f585af9d66dce1219e533caffdea4d6e0fcb183a
ラッキー <60083402+lucky78653455@users.noreply.github.com>
2020-11-30 23:18:14 +0700

    Add files via upload

---
+++ AI Patrol (movement).cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Patrol : MonoBehaviour
+{
+
+    public float speed;
+    private float waitTime;
+    public float startWaitTime;
+
+    public Transform moveSpots;
+    public float minX;
+    public float maxX;
+    public float minY;
+    public float maxY;
+
+    // Use this for initialization
+    void Start()
+    {
+        waitTime = startWaitTime;
+        moveSpots.position = new Vector2(Random.Range(minX, maxX), Random.Range(minY, maxY));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        transform.position = Vector2.MoveTowards(transform.position, moveSpots.position, speed * Time.deltaTime);
+
+        if (Vector2.Distance(transform.position, moveSpots.position) < 0.2f)
+        {
+            if (waitTime <= 0)
+            {
+                moveSpots.position = new Vector2(Random.Range(minX, maxX), Random.Range(minY, maxY));
+                waitTime = startWaitTime;
+            }
+            else
+            {
+                waitTime -= Time.deltaTime;
+            }
+        }
+    }
+}
